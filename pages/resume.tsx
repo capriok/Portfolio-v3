a09a686cd5b03cfda0f55d42371254c8811e0632
capriok <capriok7@gmail.com>
2022-06-11 09:46:38 -0700

    resume fallback

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,31 +1,43 @@
-import SEO from '@components/Seo'
-import React from 'react'
-import Layout from '../components/layouts/ResumeLayout'
+import SEO from "@components/Seo";
+import React from "react";
+import Layout from "../components/layouts/ResumeLayout";
 
-import styles from '../styles/components/resume.module.scss'
+import styles from "../styles/components/resume.module.scss";
 
-const PDF = process.env.NEXT_PUBLIC_RESUME
+const PrimaryPDF = process.env.NEXT_PUBLIC_RESUME;
+const FallbackPDF = process.env.NEXT_PUBLIC_RESUME_DIRECT;
 
 const Resume: React.FC = () => (
-	<>
-		<SEO title="Resume" />
-		<Layout>
-			<div className={styles.resume}>
-				<object
-					data={PDF}
-					type="application/pdf"
-					width="100%"
-					height="100%">
-					Resume
-				</object>
-			</div>
-			<div className={styles.resumeMobile}>
-				<button className={styles.resumeBtn} onClick={() => window.open(PDF)}>
-					View Resume
-				</button>
-			</div>
-		</Layout>
-	</>
-)
+  <>
+    <SEO title="Resume" />
+    <Layout>
+      <div className={styles.resume}>
+        <object
+          data={PrimaryPDF}
+          type="application/pdf"
+          width="100%"
+          height="100%"
+        >
+          <object
+            data={FallbackPDF}
+            type="application/pdf"
+            width="100%"
+            height="100%"
+          >
+            Resume
+          </object>
+        </object>
+      </div>
+      <div className={styles.resumeMobile}>
+        <button
+          className={styles.resumeBtn}
+          onClick={() => window.open(PrimaryPDF)}
+        >
+          View Resume
+        </button>
+      </div>
+    </Layout>
+  </>
+);
 
-export default Resume
+export default Resume;
