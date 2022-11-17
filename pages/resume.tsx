a87a8e58597b90342cb50bc4fd6173a632f4cbfd
capriok <capriok7@gmail.com>
2022-11-17 15:34:42 -0700

    fix fallback, update data

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -4,20 +4,24 @@ import Layout from "../components/layouts/ResumeLayout";
 
 import styles from "../styles/components/resume.module.scss";
 
-const PrimaryPDF = process.env.NEXT_PUBLIC_RESUME_DIRECT;
+const PrimaryPDF = process.env.NEXT_PUBLIC_RESUME;
+const FallbackPDF = process.env.NEXT_PUBLIC_RESUME_DIRECT;
+
+const props = {
+  type: "application/pdf",
+  width: "100%",
+  height: "100%",
+};
 
 const Resume: React.FC = () => (
   <>
     <SEO title="Resume" />
     <Layout>
       <div className={styles.resume}>
-        <object
-          data={PrimaryPDF}
-          type="application/pdf"
-          width="100%"
-          height="100%"
-        >
-          Resume
+        <object data={PrimaryPDF} {...props}>
+          <object data={FallbackPDF} {...props}>
+            Resume
+          </object>
         </object>
       </div>
       <div className={styles.resumeMobile}>
